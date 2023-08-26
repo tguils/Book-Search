c011d23bad6324d287e9dc4f9ba2e232673f5991
tguils <tess.guilmette@gmail.com>
2023-08-26 10:50:34 -0400

    App.js: Create an Apollo Provider to make every request work with the Apollo server.

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,60 @@
 import React from 'react';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+//class activity 25-Ins_Resolver-Context used for this page 
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
+    <ApolloProvider client={client}>
     <Router>
       <>
         <Navbar />
-        <Switch>
-          <Route exact path='/' component={SearchBooks} />
-          <Route exact path='/saved' component={SavedBooks} />
-          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-        </Switch>
+        <Routes>
+          <Route  
+          path='/' 
+          element={<SearchBooks />} 
+          />
+          <Route  
+          path='/saved' 
+          component={<SavedBooks />}
+           />
+          <Route 
+          render={() => <h1 className='display-2'>Wrong page!</h1>} 
+          />
+        </Routes>
       </>
     </Router>
+    </ApolloProvider>
   );
 }
 
