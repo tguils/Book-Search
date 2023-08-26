b8859fbbe4594d3147a91c216a59e07d887be6d1
tguils <tess.guilmette@gmail.com>
2023-08-26 10:09:41 -0400

    working on server side schemas and js. bugs with signing up and logging in still - on client side perhaps.

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -13,7 +13,56 @@ const resolvers = {
       },
 
     Mutation: {
-       
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+      
+            if (!user) {
+              throw new AuthenticationError('No user with this email found!');
+            }
+      
+            const correctPw = await user.isCorrectPassword(password);
+      
+            if (!correctPw) {
+              throw new AuthenticationError('Incorrect password!');
+            }
+      
+            const token = signToken(user);
+            return { token, user };
+          },
+      
+          addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+            
+            return { token, user};
+          },
+          saveBook: async (parent, book, context) => {
+           
+            if (context.user) {
+              return User.findOneAndUpdate(
+                { _id: context.user._id},
+                {
+                  $addToSet: { savedBooks: book},
+                },
+                {
+                  new: true,
+                  runValidators: true 
+                }
+              );
+            }
+    
+            throw new AuthenticationError('You need to be logged in!');
+          },
+          removeBook: async (parent, { bookId }, context) => {
+            if (context.user) {
+              return User.findOneAndUpdate(
+                { _id: context.user._id },
+                { $pull: { savedBooks: { bookId: bookId } } },
+                { new: true }
+              );
+            }
+            throw new AuthenticationError('You need to be logged in!');
+          },
         },
     
     };
