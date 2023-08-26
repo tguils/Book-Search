998dd6abef56046ba9c6a65b8d106053b3bc0d52
tguils <tess.guilmette@gmail.com>
2023-08-26 10:58:32 -0400

    mutations.js:
    LOGIN_USER will execute the loginUser mutation set up using Apollo Server.
    
    ADD_USER will execute the addUser mutation.
    
    SAVE_BOOK will execute the saveBook mutation.
    
    REMOVE_BOOK will execute the removeBook mutation.

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,13 +1,69 @@
 import { gql } from '@apollo/client';
 //login user should take in name email and password then returns token and name 
-export const LOGIN_USER = gql ``;
+export const LOGIN_USER = gql `
+    mutation loginUser($email: String!, $password: String!) {
+        loginUser(email: &email, password: $password) {
+            token
+            user {
+                _id
+                username
+                email
+            }
+        }
+    }
+`;
 
 //add user should take in name email password and return adds them and then returns id, name and email and token
-export const ADD_USER = gql ``;
+export const ADD_USER = gql `
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+                email
+            }
+        }
+    }
+`;
 
 //save book should take in all the book variables and then returns the user and user's saved books 
-export const SAVE_BOOK = gql ``;
+export const SAVE_BOOK = gql `
+    mutation saveBook($bookId: String!, $description: String!, $title: String! $authors: [String], $image: String, $link: String) {
+        saveBook(bookId: $bookId, description: $description, title: $title authors: $authors, image: $image, link: $link) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                title
+                description
+                authors
+                image
+                link
+            }
+        }
+    }
+`;
 
 
 //This will take in the book variables and then the return the user's updated saved books minus the book details on teh deleted one
-export const REMOVE_BOOK = gql ``;
+export const REMOVE_BOOK = gql `
+mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+            bookId
+            title
+            description
+            authors
+            image
+            link
+        }
+    }
+}
+`;
