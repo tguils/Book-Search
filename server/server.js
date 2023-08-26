f58d61c02608b7f0484aa4759853af56812ed687
tguils <tess.guilmette@gmail.com>
2023-08-25 20:57:09 -0400

    working with server.js to get middleware and graphql running. Importing typeDefs and Resolvers fixes.

--- server/server.js
+++ server/server.js
@@ -1,23 +1,22 @@
 const express = require('express');
 const path = require('path');
-const db = require('./config/connection');
 const { ApolloServer } = require("apollo-server-express");
+const db = require('./config/connection');
 
 const { typeDefs, resolvers } = require("./schemas");
-const { authMiddleware } = require("./utils/auth");
+const { authMiddleware } = require('./utils/auth');
 
-const app = express();
-const PORT = process.env.PORT || 3001;
 
+// const routes = require('./routes');
+app.use(authMiddleware);
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware,
 });
-
-server.applyMiddleware({ app });
-
+const PORT = process.env.PORT || 3001;
+const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -27,10 +26,23 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
+// app.use(routes);
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
+
+  server.applyMiddleware({ app });
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    });
+  });
+
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
+
